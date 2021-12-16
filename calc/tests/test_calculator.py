b7e90b291a832b7d7db46da5dacaec81938f9915
@rudysemola <r.semola@studenti.unipi.it>
2021-12-16 10:41:17 +0100

    FIX bug in calculator.py and unit test DONE

--- calc/tests/test_calculator.py
+++ calc/tests/test_calculator.py
@@ -60,6 +60,37 @@ class TestAdd(unittest.TestCase):
     def test_divide_by_zero(self):
         self.assertRaises(ZeroDivisionError, c.divide, 6, 0)
 
+    "RS test"
+    def test_multiply_integers_positive(self):
+        result = c.multiply(5, 5)
+        self.assertEqual(result, 25)
+
+    def test_multiply_integers_negative(self):
+        result = c.multiply(-5, -10)
+        self.assertEqual(result, 50)
+    
+    def test_multiply_integers_pos_neg(self):
+        result = c.multiply(2, -10)
+        self.assertEqual(result, -20)
+
+    def test_multiply_integers_neg_pos(self):
+        result = c.multiply(-2, 10)
+        self.assertEqual(result, -20)
+
+    def test_multiply_integers_zero(self):
+        result1 = c.multiply(-2, 0)
+        self.assertEqual(result1, 0)
+        result2 = c.multiply(0, 2)
+        self.assertEqual(result2, 0)
+
+    def test_subtract_integers_positive(self):
+        result = c.subtract(10, 6)
+        self.assertEqual(result, 4)
+
+    def test_subtract_integers_negative(self):
+        result = c.subtract(2, 5)
+        self.assertEqual(result, -3)
+
 
 if __name__ == "__main__":
     unittest.main()
